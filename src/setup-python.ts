1119fe7469e89bbd33b3ad27e26d08aa730d4542
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2026-08-14 17:08:12 +0200

    Remove PyPy version handling from setup-python.ts
    Removed PyPy version handling and related symlink logic.

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -8,27 +8,8 @@ import { symlinkSync, readdirSync, readdir, existsSync } from "fs"
 const IS_WINDOWS = process.platform === "win32"
 
 const allCPythonVersions = findAllVersions("Python")
-const allPyPyVersions = findAllVersions("PyPy")
 
 console.log("Available CPython versions:", allCPythonVersions)
-console.log("Available PyPy versions:", allPyPyVersions)
-
-function symlinkIfNotExistsSync(target: string, link: string) {
-  if (!existsSync(link)) symlinkSync(target, link)
-}
-
-for (const version of allPyPyVersions) {
-  const root = findVersion("PyPy", version)
-  addPath(`${root}/bin`)
-  if (/^2\./.test(version)) {
-    let minorVersion = /^2\.\d+/.exec(version)![0]
-    symlinkIfNotExistsSync(`${root}/bin/pypy`, `${root}/bin/pypy2`)
-    symlinkIfNotExistsSync(
-      `${root}/bin/pypy`,
-      `${root}/bin/pypy${minorVersion}`,
-    )
-  }
-}
 
 for (const version of allCPythonVersions) {
   const root = findVersion("Python", version)
