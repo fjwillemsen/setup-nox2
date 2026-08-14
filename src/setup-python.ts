68025d4312f5f61cd3a92976e42a1367066b652d
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2026-08-14 17:12:06 +0200

    Format and clean up setup-python.ts code

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -1,37 +1,37 @@
-import { findAllVersions, find as findVersion } from "@actions/tool-cache"
-import { addPath } from "@actions/core"
-import { execSync } from "child_process"
-import { symlinkSync, readdirSync, readdir, existsSync } from "fs"
+import { findAllVersions, find as findVersion } from "@actions/tool-cache";
+import { addPath } from "@actions/core";
+import { execSync } from "child_process";
+import { symlinkSync, readdirSync, readdir, existsSync } from "fs";
 
 //function installPythonVersion(type: "Python" | "PyPy")
 
-const IS_WINDOWS = process.platform === "win32"
+const IS_WINDOWS = process.platform === "win32";
 
-const allCPythonVersions = findAllVersions("Python")
+const allCPythonVersions = findAllVersions("Python");
 
-console.log("Available CPython versions:", allCPythonVersions)
+console.log("Available CPython versions:", allCPythonVersions);
 
 for (const version of allCPythonVersions) {
-  const root = findVersion("Python", version)
+  const root = findVersion("Python", version);
   if (IS_WINDOWS) {
-    addPath(root)
-    addPath(`${root}\\Scripts`)
+    addPath(root);
+    addPath(`${root}\\Scripts`);
   } else {
-    addPath(`${root}/bin`)
+    addPath(`${root}/bin`);
   }
 }
 
-const NOX_PYTHON_VERSION = allCPythonVersions[allCPythonVersions.length - 1]
-console.log("Nox itself will be installed using", NOX_PYTHON_VERSION)
+const NOX_PYTHON_VERSION = allCPythonVersions[allCPythonVersions.length - 1];
+console.log("Nox itself will be installed using", NOX_PYTHON_VERSION);
 const NOX_PYTHON_PATH =
   findVersion("Python", NOX_PYTHON_VERSION) +
-  (IS_WINDOWS ? "\\python.exe" : "/bin/python")
+  (IS_WINDOWS ? "\\python.exe" : "/bin/python");
 
 const NOX_BIN_PATH = execSync(
   `${NOX_PYTHON_PATH} -c "import os, sysconfig; print(sysconfig.get_path('scripts', f'{os.name}_user'))"`,
 )
   .toString()
-  .trim()
+  .trim();
 
-addPath(NOX_BIN_PATH)
-execSync(`${NOX_PYTHON_PATH} -m pip install --user nox`)
+addPath(NOX_BIN_PATH);
+execSync(`${NOX_PYTHON_PATH} -m pip install --user nox`);
